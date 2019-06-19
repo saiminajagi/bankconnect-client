ecf5c57e9326320d13d709ed5c1c50559b72b8df
Sai Kumar <saikum15@in.ibm.com>
2019-06-19 12:40:04 +0530

    donno

--- bankconnect-client/server/index.js
+++ bankconnect-client/server/index.js
@@ -7,9 +7,10 @@ var routes = require('./routes/routes');
 var posts = require('./routes/posts');
 
 var mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/bcclient',{useNewUrlParser: true},(err,db)=>{
- 
+
     if(err) console.log(err);
     else console.log("connection to db success");
 });
@@ -54,7 +55,7 @@ setInterval(() => {
                 var ts = parseInt(doc[i].ts);
                 if(cts - ts >= 86400000){
                     //if the time is more than a day.
-                    usermodel.findOneAndUpdate({ts: ts},{$set : {ts : "expired"}},{new : true});  
+                    usermodel.findOneAndUpdate({ts: ts},{$set : {ts : "expired"}},{new : true});
                 }
             }
         }
