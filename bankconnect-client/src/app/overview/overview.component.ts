dc4c6cfde431d1686c314dad259b58457e0bb564
Sai Kumar <saikum15@in.ibm.com>
2019-07-02 15:47:21 +0530

    mine

--- bankconnect-client/src/app/overview/overview.component.ts
+++ bankconnect-client/src/app/overview/overview.component.ts
@@ -10,33 +10,33 @@ import { SignupServiceService } from '../services/signup-service.service';
 })
 export class OverviewComponent implements OnInit {
 
-  apiname:string;
-  name = "default";
-  desc = "default";
-  key_features = "default";
-  use_cases = "default";
+  apiname: string;
+  name = 'default';
+  desc = 'default';
+  key_features = [];
+  use_cases = [];
   apiDetails: any;
 
-  constructor(private router: Router,private route: ActivatedRoute,private signservice : SignupServiceService) {
+  constructor(private router: Router, private route: ActivatedRoute, private signservice: SignupServiceService) {
 
-    this.route.params.subscribe( params => {
-      console.log("Api name is: "+params['apiname']);
+    this.route.params.subscribe(params => {
+      console.log('Api name is: ' + params['apiname']);
       var myObj = {
-        api : params['apiname']
+        api: params['apiname']
       }
 
-    this.signservice.getApiDetails(myObj)
-    .subscribe((data)=>{
-      console.log("data is: "+data);
-      this.apiDetails = data;
-      this.name = data.name;
-      this.desc = data.desc;
-      this.key_features = data.key_features;
-      this.use_cases = data.use_cases;
+      this.signservice.getApiDetails(myObj)
+        .subscribe((data) => {
+          console.log('data is: ' + data);
+          this.apiDetails = data;
+          this.name = data.name;
+          this.desc = data.desc;
+          this.key_features = data.key_features;
+          this.use_cases = data.use_cases;
 
-    },(err)=> console.log(err));
+        }, (err) => console.log(err));
     })
-   }
+  }
 
   ngOnInit() {
   }
